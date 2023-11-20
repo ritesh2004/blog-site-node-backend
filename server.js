4ad230ada582755847d5e3dbeb9e55d9b5888051
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-11-20 11:16:18 +0530

    home url configured

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import connectDB from "./data/database.js";
 
 connectDB()
 
+app.get('/',(req,res)=>{
+    res.send("Nice! Working")
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server is working in ${process.env.NODE_ENV} mode at port: ${process.env.PORT}`)
 })
