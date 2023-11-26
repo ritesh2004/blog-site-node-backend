c30f9f74cfb841d7c6521acb3953c8ade4e8ad12
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-11-26 21:54:57 +0530

    log.json file handler added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/err.js";
 import cors from 'cors';
+import fs from 'fs';
+import moment from "moment";
 
 export const app = express();
 config({
@@ -24,3 +26,19 @@ app.use("/api/v1/",userRouter)
 app.use("/api/v1/",blogRouter)
 
 app.use(errorMiddleware)
+
+app.use((req,res,next)=>{
+    const logEntry = {
+        method : req.method,
+        time : moment().format('YYYY-MM-DD HH:mm:ss'),
+        apiAddress : req.url,
+        ipAddress : req.ip
+    }
+
+    fs.appendFile('log.json',JSON.stringify(logEntry)+'\n',(err)=>{
+        if (err) {
+            console.error("Error writing to log file: ",err)
+        }
+    })
+    next()
+})
